418f122c664eb154e99a9c0b3e8345a95420433b
bipul <bipulkansakar@gmail.com>
2019-12-10 15:02:08 +0545

    This is my 2nd Assignment on Android.

---
+++ app/src/main/java/com/bipulproject/fragment2ndassignment/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bipulproject.fragment2ndassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
